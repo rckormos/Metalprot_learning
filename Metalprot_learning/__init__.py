23f22eb0f5743080486645d8cd96c3e1fa048e8e
jzhang1198 <jzhang1198@gmail.com>
2022-04-21 11:04:46 -0700

    Update

--- Metalprot_learning/__init__.py
+++ Metalprot_learning/__init__.py
@@ -4,6 +4,6 @@ DeGrado Lab 2021
 """
 
 from .get_binding_cores import construct_training_example
-from .metalprot_model1 import SingleLayerNet
+from .models import SingleLayerNet, train_model
 
 __version__ = '0.1.0'
---
+++ job_scripts/train.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Author: Jonathan Zhang <jon.zhang@ucsf.edu>
+
+
+"""
+
+#imports
+from Metalprot_learning import *
+
+if __name__ == '__main__':
+    path2features = ''
+
+    pass
