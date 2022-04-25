1ed5aa25243082ab6f367c60e58d2ea8c0a92f38
jzhang1198 <jzhang1198@gmail.com>
2022-04-25 12:48:04 -0700

    Update

--- Metalprot_learning/data.py
+++ Metalprot_learning/data.py
@@ -4,7 +4,7 @@ Author: Jonathan Zhang <jon.zhang@ucsf.edu>
 This file contains functions for preprocessing and manipulating data for model training.
 """
 
-#imports 
+#imports
 import os
 import pickle
 import numpy as np
@@ -18,7 +18,7 @@ class DistanceData(torch.utils.data.Dataset):
         self.observations = observations
 
     def __len__(self):
-            return len(self.labels)
+        return len(self.labels)
 
     def __getitem__(self, index):
         observation = self.observations[index]
@@ -39,33 +39,31 @@ def load_data(path2features: str):
     Y = []
     
     feature_files = [os.path.join(path2features, file) for file in os.listdir(path2features) if 'features.pkl' in file] #extract feature files
-    counter = 0
-    for file in feature_files:
-        counter += 1
+    for iteration, file in enumerate(feature_files):
         with open(file, 'rb') as f:
             data = pickle.load(f)
 
-        x = list(data['full_observations']) #get observations for all permutations of core
-        y = list(data['full_labels'])
+        if iteration == 0:
+            X = data['full_observations']
+            Y = data['full_labels']
 
-        X.append(x)
-        Y.append(y)
-
-    X = np.array(X)
-    Y = np.array(Y)
+        else:
+            X = np.vstack([X, data['full_observations']])
+            Y = np.vstack([Y, data['full_labels']])
 
     np.savetxt(os.path.join(path2features, 'observations.txt'), X)
     np.savetxt(os.path.join(path2features, 'labels.txt'), Y)    
 
     return X, Y    
 
-def split_data(X: np.ndarray, y: np.ndarray, train_prop=0.8):
+def split_data(X: np.ndarray, y: np.ndarray, train_prop=0.8, seed=42):
     """Splits data into training and test sets.
 
     Args:
         X (np.ndarray): Observation data.
         y (np.ndarray): Label data.
         train_size (float, optional): The proportion of data to be paritioned into the training set. Defaults to 0.8.
+        seed (int, optional): The random seed for splitting. Defaults to 42.
 
     Returns:
         training_data (__main__.DistanceData): Dataset object of training data.
@@ -73,7 +71,7 @@ def split_data(X: np.ndarray, y: np.ndarray, train_prop=0.8):
     """
 
     training_size = int(train_prop * X.shape[0])
-    indices = np.random.permutation(X.shape[0])
+    indices = np.random.RandomState(seed=seed).permutation(X.shape[0])
     training_indices, val_indices = indices[:training_size], indices[training_size:]
     X_train, y_train, X_val, y_val = X[training_indices], y[training_indices], X[val_indices], y[val_indices]
     training_data, validation_data = DistanceData(X_train, y_train), DistanceData(X_val, y_val)
