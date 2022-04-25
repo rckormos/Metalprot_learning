03b37a4944ef9f80ae03e82d2f27b5e96bc0a05e
jzhang1198 <jzhang1198@gmail.com>
2022-04-25 14:27:47 -0700

    Update

--- job_scripts/train.py
+++ job_scripts/train.py
@@ -3,13 +3,26 @@
 """
 Author: Jonathan Zhang <jon.zhang@ucsf.edu>
 
-
+This script runs model training. 
 """
 
 #imports
-from Metalprot_learning import *
+from Metalprot_learning.models import *
 
 if __name__ == '__main__':
-    path2features = ''
+    path2observations = ''
+    path2labels = ''
 
-    pass
+    #define hyperparameters
+    epochs = 1000
+    batch_size = 4
+    learning_rate = 0.001
+    loss_function = 'MAE' #can be mean absolute error (MAE) or mean squared error (MSE)
+    optimizer = 'SGD' #currently can only be stochastic gradient descent (SGD)
+    training_proportion = 0.8
+    seed = 42
+    filename = None
+
+    model = SingleLayerNet()
+    train_model(model, path2observations, path2labels, epochs, batch_size, learning_rate, loss_function, 
+                optimizer, filename=filename, train_prop=training_proportion, seed=seed)
