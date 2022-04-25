d055b6c775f0fe1f6338883a50c66e9878ae7629
jzhang1198 <jzhang1198@gmail.com>
2022-04-25 14:18:05 -0700

    No longer needed

--- Metalprot_learning/data.py
+++ Metalprot_learning/data.py
@@ -23,38 +23,7 @@ class DistanceData(torch.utils.data.Dataset):
     def __getitem__(self, index):
         observation = self.observations[index]
         label = self.labels[index]
-        return observation, label
-
-def load_data(path2features: str):
-    """Reads in pickle files containing features for cores and writes them into model-readable form.
-
-    Args:
-        path2features (str): Directory containing feature files.
-
-    Returns:
-        X (np.ndarray): 
-        Y (np.ndarray): 
-    """
-    X = []
-    Y = []
-    
-    feature_files = [os.path.join(path2features, file) for file in os.listdir(path2features) if 'features.pkl' in file] #extract feature files
-    for iteration, file in enumerate(feature_files):
-        with open(file, 'rb') as f:
-            data = pickle.load(f)
-
-        if iteration == 0:
-            X = data['full_observations']
-            Y = data['full_labels']
-
-        else:
-            X = np.vstack([X, data['full_observations']])
-            Y = np.vstack([Y, data['full_labels']])
-
-    np.savetxt(os.path.join(path2features, 'observations.txt'), X)
-    np.savetxt(os.path.join(path2features, 'labels.txt'), Y)    
-
-    return X, Y    
+        return observation, label 
 
 def split_data(X: np.ndarray, y: np.ndarray, train_prop=0.8, seed=42):
     """Splits data into training and test sets.
