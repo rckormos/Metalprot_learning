79631d06d5dc324c31ebd0b031fcbd567eb9e8fe
jzhang1198 <jzhang1198@gmail.com>
2022-04-25 13:42:42 -0700

    Fixed syntaxing

--- Metalprot_learning/models.py
+++ Metalprot_learning/models.py
@@ -4,40 +4,31 @@ Author: Jonathan Zhang <jon.zhang@ucsf.edu>
 This file contains the architecture of the metalprot_learning model version 1. This model performs prediction of backbone distances to a metal.
 """
 
+#imports
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
 
 class SingleLayerNet(nn.Module):
-    def __init__(self, epochs: int, lr: float, batch_size: int, loss_fn: str, optimizer: str):
+    def __init__(self):
         super(SingleLayerNet, self).__init__()
-        self.epochs = epochs
-        self.batch_size = batch_size
-        self.lr = lr
-        self.optimizer = optimizer
-
-        loss_fn_dict = {'MAE': nn.L1Loss(),
-                        'MSE': nn.MSELoss()}
-        self.loss_fn = loss_fn_dict[loss_fn]
 
         self.block1 = nn.Sequential(
-            nn.Linear(384, 9),
+            nn.Linear(2544, 48),
             nn.ReLU(),
         )
 
     def forward(self, x):
-        x = self.flatten(x)
-        y = self.block1(x)
+        y = self.block1(x.float())
         return y
 
-def train_loop(model, train_dataloader, optimizer):
+def train_loop(model, train_dataloader, loss_fn, optimizer):
 
     for batch, (X, y) in enumerate(train_dataloader):
         
         #make prediction
         prediction = model.forward(X) 
-        loss = model.loss_fn(prediction, y)
+        loss = loss_fn(prediction, y)
 
         #backpropagate and update weights
         optimizer.zero_grad()
@@ -46,31 +37,53 @@ def train_loop(model, train_dataloader, optimizer):
 
     return loss.item()
 
-def validation_loop(model, validation_dataloader):
+def validation_loop(model, validation_dataloader, loss_fn):
     validation_loss = 0
     with torch.no_grad():
         for X,y in validation_dataloader:
             prediction = model(X)
-            validation_loss += model.loss_fn(prediction,y).item()
+            validation_loss += loss_fn(prediction,y).item()
 
     validation_loss /= len(validation_dataloader)
     return validation_loss
 
-def train_model(model, train_data, test_data):
+def train_model(model, train_data, test_data, epochs: int, batch_size: int, lr: float, loss_fn: str, optimizer: str, filename=None):
+    """Runs model training.
 
-    train_dataloader = DataLoader(train_data, batch_size=model.batch_size, shuffle=True)
-    validation_dataloader = DataLoader(test_data, batch_size=model.batch_size, shuffle=True)
+    Args:
+        model (SingleLayerNet): SingleLayerNet object.
+        train_data: DataLoader object containing training data.
+        test_data: Dataloader object containing validation data.
+        batch_size (int): Batch size for training.
+        lr (float): Learning rate for training.
+        loss_fn (str): Defines the loss function for backpropagation. Must be a string in {'MAE', 'MSE'}
+        optimizer (str): Defines the optimization algorithm for backpropagation. Must be a string in {'SGD'}
+    """
 
-    optimizer_dict = {'SGD': torch.optim.SGD(model.parameters(), lr=model.lr)}
-    optimizer = optimizer_dict[model.optimizer]
+    #instantiate dataloader objects for train and test sets
+    train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    validation_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+
+    #define optimizer and loss function
+    optimizer_dict = {'SGD': torch.optim.SGD(model.parameters(), lr=lr)}
+    optimizer = optimizer_dict[optimizer]
+
+    loss_fn_dict = {'MAE': torch.nn.L1Loss(),
+                    'MSE': torch.nn.MSELoss()}
+    loss_fn = loss_fn_dict[loss_fn]
 
     train_loss =[]
     validation_loss = []
-    for epoch in range(0, model.epochs):
-        _train_loss = train_loop(model, train_dataloader, optimizer)
-        _validation_loss, _accuracy = validation_loop(model, validation_dataloader)
+    for epoch in range(0, epochs):
+        _train_loss = train_loop(model, train_dataloader, loss_fn, optimizer)
+        _validation_loss = validation_loop(model, validation_dataloader, loss_fn)
 
         train_loss.append(_train_loss)
-        validation_loss.append(validation_loss)
+        validation_loss.append(_validation_loss)
+
+    if filename:
+        torch.save(model.state_dict(), filename + '.pth')
+
+    return train_loss, validation_loss
 
     
