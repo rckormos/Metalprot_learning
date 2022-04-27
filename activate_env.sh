4b74d6c490db439083d4e4c1e47c58f071fea441
Jonathan Zhang <jzhang1198@dev1.wynton.ucsf.edu>
2022-04-26 18:47:19 -0700

    Changed paths for Wynton

--- activate_env.sh
+++ activate_env.sh
@@ -5,6 +5,6 @@
 
 # The sole purpose of this bash script is to set up the virtual environment to run your jobs in. 
 
-conda activate proteindesign
+conda activate Metalprot_learning
 
 $@ ${SGE_TASK_ID}
