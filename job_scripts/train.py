fe5900ac414cbdfe9a216f080104acdf86dac360
Jonathan Zhang <jzhang1198@dev1.wynton.ucsf.edu>
2022-04-29 09:13:34 -0700

    Param update for Wynton run

--- job_scripts/train.py
+++ job_scripts/train.py
@@ -8,10 +8,20 @@ This script runs model training.
 
 #imports
 from Metalprot_learning.models import *
+import sys
 
 if __name__ == '__main__':
-    path2observations = '/Users/jonathanzhang/Documents/ucsf/degrado/data/observations.npy'
-    path2labels = '/Users/jonathanzhang/Documents/ucsf/degrado/data/labels.npy'
+
+    path2output = sys.argv[1] #path to store outputs    
+    no_jobs = 1
+    job_id = 0
+
+    if len(sys.argv) > 3:
+        no_jobs = int(sys.argv[2])
+        job_id = int(sys.argv[3]) - 1
+
+    path2observations = '/wynton/home/rotation/jzhang1198/data/metalprot_learning/ZN_binding_cores/observations.npy'
+    path2labels = '/wynton/home/rotation/jzhang1198/data/metalprot_learning/ZN_binding_cores/labels.npy'
 
     #define architecture of neural network
     arch = [{'input_dim': 2544, 'output_dim': 1272, 'activation': None}, 
@@ -22,14 +32,14 @@ if __name__ == '__main__':
 
     #define hyperparameters
     epochs = 1000
-    batch_size = 4
+    batch_size = 2000
     learning_rate = 0.001
     loss_function = 'MAE' #can be mean absolute error (MAE) or mean squared error (MSE)
     optimizer = 'SGD' #currently can only be stochastic gradient descent (SGD)
     partition = (0.8,0.1,0.1)
     seed = 42
-    filename = '/Users/jonathanzhang/Documents/ucsf/degrado/data/deezma.pth'
+    name = 'model1'
 
     model = SingleLayerNet(arch)
-    train_model(model, path2observations, path2labels, epochs, batch_size, learning_rate, loss_function, 
-                optimizer, filename=filename, partition=partition, seed=seed)
+    train_model(model, path2output, path2observations, path2labels, epochs, batch_size, learning_rate, loss_function, 
+                optimizer, name=name, partition=partition, seed=seed)
