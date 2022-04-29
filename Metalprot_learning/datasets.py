e5dcde2827688036ef6fe7fbb38e81c9bd00d96a
Jonathan Zhang <jzhang1198@dev1.wynton.ucsf.edu>
2022-04-29 11:00:48 -0700

    Added dedicated file for dataset functions

--- Metalprot_learning/datasets.py
+++ Metalprot_learning/datasets.py
@@ -23,32 +23,38 @@ class DistanceData(torch.utils.data.Dataset):
         label = self.labels[index]
         return observation, label 
 
-def split_data(X, y, partitions, seed):
+def split_data(observation_file: str, label_file: str, partitions: tuple, seed: int):
     """Splits data into training and test sets.
 
     Args:
-        X (np.ndarray): Observation data.
-        y (np.ndarray): Label data.
+        observation_file (str): Path to observations.npy file.
+        label_file (str): Path to labels.npy file.
         partitions (tuple): Tuple containing proportion to partition into training, testing, and validation sets respectively.
         seed (int): The random seed for splitting.
 
     Returns:
         training_data (__main__.DistanceData): Dataset object of training data.
+        testing_data (__main__.DistanceData): Dataset object of testing data.
         validation_data (__main__.DistanceData): Dataset object of validation data.
     """
 
+    #load data
+    X = np.load(observation_file)
+    y = np.load(label_file)
+
+    #define data partitions
     train_prop, test_prop, val_prop = partitions
     assert sum([train_prop, test_prop, val_prop]) == 1
-
     training_size = int(train_prop * X.shape[0])
     testing_size = int(test_prop * X.shape[0])
     validation_size = int(val_prop * X.shape[0])
 
+    #randomly assign examples to training sets
     indices = np.random.RandomState(seed=seed).permutation(X.shape[0])
     training_indices, test_indices, val_indices = indices[:training_size], indices[training_size:(training_size+testing_size)], indices[(training_size + testing_size):] 
     X_train, y_train, X_test, y_test, X_val, y_val = X[training_indices], y[training_indices], X[test_indices], y[test_indices], X[val_indices], y[val_indices]
     assert sum([i.shape[0] for i in [X_train, X_test, X_val]]) == sum([i.shape[0] for i in [y_train, y_test, y_val]]) == X.shape[0]
 
-    training_data, testing_data = DistanceData(X_train, y_train), DistanceData(X_test, y_test)
+    training_data, testing_data, validation_data = DistanceData(X_train, y_train), DistanceData(X_test, y_test), DistanceData(X_val, y_val)
 
-    return training_data, testing_data, X_val, y_val
+    return training_data, testing_data, validation_data
