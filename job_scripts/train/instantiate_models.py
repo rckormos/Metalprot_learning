787a8bccfc7ca517ef46f86788b143ee678823eb
Jonathan Zhang <jzhang1198@dev1.wynton.ucsf.edu>
2022-05-10 13:38:19 -0700

    Reorganized training job script

---
+++ job_scripts/train/instantiate_models.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+"""
+Author: Jonathan Zhang <jon.zhang@ucsf.edu>
+
+This script creates directories and architecture files that specify model hyperparameters.
+"""
+
+#impots 
+import sys
+import os
+import json
+
+def enumerate_models(arch: dict, epochs: list, batch_sizes: list, learning_rates: list, loss_functions: list, optimizers: list, partition: tuple, seed: int):
+    """Distributes batch jobs accross mutliple cores.
+
+    Args:
+        arch (dict): Dictionary defining model architecture.
+        epochs (list): List of epochs.
+        batch_size (list): List of batch sizes.
+        learning_rate (list): List of learning rates.
+        loss_function (list): List of loss functions to be tested.
+        optimizer (list): List of optimizers to be tested.
+        partition (tuple): Defines partitioning of data into training, testing, and validation sets.
+        seed (int): Random seed for reproducibility.
+
+    Returns:
+        combinations (list): List of tuples containing hyperparameter combinations.
+    """
+
+    combinations = list(set([(i,j,k,l,m) for i in epochs for j in batch_sizes for k in learning_rates for l in loss_functions for m in optimizers]))
+    combinations.sort()    
+
+    for combination in combinations:
+        #make directory to hold training hyperparameters
+        name = '_'.join([str(i) for i in list(combination)])
+        model_dir = os.path.join(path2output, name)
+        os.makedirs(model_dir)
+
+        hyperparams = {}
+        hyperparams['arch'] = arch
+        hyperparams['epochs'] = combination[0]
+        hyperparams['batch_size'] = combination[1]
+        hyperparams['lr'] = combination[2]
+        hyperparams['loss_fn'] = combination[3]
+        hyperparams['optimizer'] = combination[4]
+        hyperparams['partition'] = partition
+        hyperparams['seed'] = seed
+
+        with open(os.path.join(model_dir, 'hyperparams.json'), 'w') as f:
+            json.dump(hyperparams, f)
+
+if __name__ == '__main__':
+    path2output = sys.argv[1]
+
+    #define architecture of neural network
+    arch = [{'input_dim': 2544, 'output_dim': 1272, 'activation': 'ReLU', 'dropout': None}, 
+            {'input_dim': 1272, 'output_dim': 636, 'activation': 'ReLU', 'dropout': None},
+            {'input_dim': 636, 'output_dim': 318, 'activation': 'ReLU', 'dropout': None},
+            {'input_dim': 318, 'output_dim': 48, 'activation': 'ReLU', 'dropout': None}]
+
+    #define hyperparameters. if you would like to implement a grid search, simply add more values to the lists
+    epochs = [2000]
+    batch_sizes = [1000]
+    learning_rates = [0.01]
+    loss_functions = ['MAE'] #can be mean absolute error (MAE) or mean squared error (MSE)
+    optimizers = ['SGD'] #currently can only be stochastic gradient descent (SGD)
+    partition = (0.8,0.1,0.1)
+    seed = 42
+
+    enumerate_models(arch, epochs, batch_sizes, learning_rates, loss_functions, optimizers, partition, seed)
---
+++ job_scripts/train/train_models.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+"""
+Author: Jonathan Zhang <jon.zhang@ucsf.edu>
+
+This script runs model training. 
+"""
+
+#imports
+from Metalprot_learning.trainer.model_trainer import train_model
+import sys
+import os
+import json
+
+def distribute_tasks(no_jobs: int, job_id: int, path2models: str):
+    """Distributes batch jobs accross mutliple cores.
+
+    Args:
+        no_jobs (int): Number of jobs.
+        job_id (int): Job id.
+        path2models (str): Path to directory containing model subdirectories.
+
+    Returns:
+        tasks (list): List of models to be train by task.
+    """
+
+    models = [os.path.join(path2models, i) for i in os.listdir(path2models)]
+    tasks = [models[i] for i in range(0,len(models)) if i % no_jobs == job_id]
+    return tasks
+
+def run_train(task: str, feature_file: str):
+
+    try:
+        with open(os.path.join(task, 'hyperparams.json'), 'r') as f:
+            hyperparams = json.load(f)
+
+    except:
+        print(f'No hyperparams.json file found in {task}')
+        return
+
+    arch = hyperparams['arch']
+    combination = (hyperparams['epochs'], hyperparams['batch_size'], hyperparams['lr'], hyperparams['loss_fn'], hyperparams['optimizer'])
+    partition = hyperparams['partition']
+    seed = hyperparams['seed']
+    train_model(task, arch, feature_file, partition, seed, combination)w
+
+if __name__ == '__main__':
+    path2output = sys.argv[1] #note that this is not actually where output files are written to. they are written to the model directories.
+    no_jobs = 1
+    job_id = 0
+
+    if len(sys.argv) > 3:
+        no_jobs = int(sys.argv[2])
+        job_id = int(sys.argv[3]) - 1
+
+    #provide paths to observations and labels
+    path2features = '/wynton/home/rotation/jzhang1198/data/metalprot_learning/ZN_binding_cores/datasetV1/compiled_features.pkl'
+    path2models = '/wynton/home/rotation/jzhang1198/data/metalprot_learning/models/MLP_v1'
+
+    #distribute and run tasks
+    tasks = distribute_tasks(no_jobs, job_id, path2models)
+    for task in tasks:
+        run_train(task, path2features) 
