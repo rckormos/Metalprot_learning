c2b44c0d0b51fdd5cb4ebebb2b414e3051c91a75
Jonathan Zhang <jzhang1198@dev1.wynton.ucsf.edu>
2022-05-12 15:48:56 -0700

    Accomodates new dict structure

--- Metalprot_learning/trainer/datasets.py
+++ Metalprot_learning/trainer/datasets.py
@@ -44,9 +44,10 @@ def split_data(features_file: str, partitions: tuple, seed: int):
 
     X = features['observations']
     y = features['labels']
-    permutations = features['permutations']
+    resind_permutations = features['resindex_permutations']
+    resnum_permutations = features['resnum_permutations']
+    metal_coords = features['metal_coords']
     sources = features['pointers']
-
 
     #define data partitions
     train_prop, test_prop, val_prop = partitions
@@ -61,17 +62,18 @@ def split_data(features_file: str, partitions: tuple, seed: int):
     X_train, y_train, X_test, y_test, X_val, y_val = X[training_indices], y[training_indices], X[test_indices], y[test_indices], X[val_indices], y[val_indices]
     
     train_sources, test_sources, val_sources = [sources[int(i)] for i in training_indices], [sources[int(i)] for i in test_indices], [sources[int(i)] for i in val_indices]
-    train_permutations, test_permutations, val_permutations = [permutations[int(i)] for i in training_indices], [permutations[int(i)] for i in test_indices], [permutations[int(i)] for i in val_indices]
-
+    train_metal_coords, test_metal_coords, val_metal_coords = [metal_coords[int(i)] for i in training_indices], [sources[int(i)] for i in test_indices], [sources[int(i)] for i in val_indices]
+    train_resind_permutations, test_resind_permutations, val_resind_permutations = [resind_permutations[int(i)] for i in training_indices], [resind_permutations[int(i)] for i in test_indices], [resind_permutations[int(i)] for i in val_indices]
+    train_resnum_permutations, test_resnum_permutations, val_resnum_permutations = [resnum_permutations[int(i)] for i in training_indices], [resnum_permutations[int(i)] for i in test_indices], [resnum_permutations[int(i)] for i in val_indices]
     
-    assert len({X_train.shape[0], y_train.shape[0], len(train_sources), len(train_permutations)}) == 1
-    assert len({X_test.shape[0], y_test.shape[0], len(test_sources), len(test_permutations)}) == 1
-    assert len({X_val.shape[0], y_val.shape[0], len(val_sources), len(val_permutations)}) == 1
+    assert len({X_train.shape[0], y_train.shape[0], len(train_sources), len(train_metal_coords), len(train_resind_permutations), len(train_resnum_permutations)}) == 1
+    assert len({X_test.shape[0], y_test.shape[0], len(test_sources), len(test_metal_coords), len(test_resind_permutations), len(test_resnum_permutations)}) == 1
+    assert len({X_val.shape[0], y_val.shape[0], len(val_sources), len(val_metal_coords), len(val_resind_permutations), len(val_resnum_permutations)}) == 1
     assert sum([i.shape[0] for i in [X_train, X_test, X_val]]) == sum([i.shape[0] for i in [y_train, y_test, y_val]]) == X.shape[0]
 
-    train_index = {'sources': train_sources, 'permutations': train_permutations}
-    test_index = {'sources': test_sources, 'permutations': test_permutations}
-    val_index = {'sources': val_sources, 'permutations': val_permutations}
+    train_index = {'pointers': train_sources, 'resindex_permutations': train_resind_permutations, 'resnum_permutations': train_resnum_permutations, 'metal_coords': train_metal_coords}
+    test_index = {'pointers': test_sources, 'resindex_permutations': test_resind_permutations, 'resnum_permutations': test_resnum_permutations, 'metal_coords': test_metal_coords}
+    val_index = {'pointers': val_sources, 'resindex_permutations': val_resind_permutations, 'resnum_permutations': val_resnum_permutations, 'metal_coords': val_metal_coords}
     training_data, testing_data, validation_data = (X_train, y_train, train_index), (X_test, y_test, test_index), (X_val, y_val, val_index)
 
     return training_data, testing_data, validation_data
