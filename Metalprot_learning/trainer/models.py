2cb7fc5dc542d78acd87b73b17897e87d051710c
jzhang1198 <jzhang1198@gmail.com>
2022-05-19 18:05:50 -0700

    New model classes

--- Metalprot_learning/trainer/models.py
+++ Metalprot_learning/trainer/models.py
@@ -33,4 +33,40 @@ class SingleLayerNet(torch.nn.Module):
 
     def forward(self, x):
         y = self.block1(x.float())
-        return y
+        return y 
+
+class SingleLayerNetV2(torch.nn.Module):
+    def __init__(self, input_dim: int, l1: int, l2: int, output_dim: int):
+        super(SingleLayerNet, self).__init__()
+
+        self.block1 = torch.nn.Sequential(
+            torch.nn.Linear(input_dim, l1),
+            torch.nn.ReLU(),
+            torch.nn.Linear(l1,l2),
+            torch.nn.ReLU(),
+            torch.nn.Linear(l2,output_dim),
+            torch.nn.ReLU()
+        )
+
+    def forward(self, x):
+        y = self.block1(x.float())
+        return y 
+
+class DoubleLayerNet(torch.nn.Module):
+    def __init__(self, input_dim: int, l1: int, l2: int, l3: int, output_dim: int):
+        super(SingleLayerNet, self).__init__()
+
+        self.block1 = torch.nn.Sequential(
+            torch.nn.Linear(input_dim, l1),
+            torch.nn.ReLU(),
+            torch.nn.Linear(l1,l2),
+            torch.nn.ReLU(),
+            torch.nn.Linear(l2,l3),
+            torch.nn.ReLU(),
+            torch.nn.Linear(l3,output_dim),
+            torch.nn.ReLU()
+        )
+
+    def forward(self, x):
+        y = self.block1(x.float())
+        return y 
