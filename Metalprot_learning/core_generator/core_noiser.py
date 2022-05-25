35853ceb7fb2f98e16c991609fe6cbb33a45c892
jzhang1198 <jzhang1198@gmail.com>
2022-05-24 19:10:58 -0700

    Implemented guassian noise function

--- Metalprot_learning/core_generator/core_noiser.py
+++ Metalprot_learning/core_generator/core_noiser.py
@@ -5,19 +5,41 @@ This file contains functions for applying coordinate noise to input cores.
 """
 
 #imports
-from prody import ANM
+from prody import ANM, AtomGroup
 import numpy as np
 
-def sample_from_gaussian():
-
-    pass
-
 def apply_noise(structure, core):
-    backbone = structure.select('protein and name CA O C N')
+    backbone = structure.select('protein and name CA N C O')
+    coords = backbone.getCoords()
+
     anm = ANM('structure ANM analysis')
     anm.buildHessian(backbone)
+    anm.calcModes(len(backbone.getResnums()), zeros=True)
+    eigenvals = anm.getEigvals().round(3)
+    eigenvecs = anm.getEigvecs().round(3)
 
+    R = 1.9872 * 1e-3 #units of kcal/ mol * K
+    T = 298 #units of K
+
+    delta_zeta = np.array([])
+    for val, coord in zip(eigenvals, np.dot(eigenvecs.T, coords.flatten())):
+
+        if val == 0:
+            delta_zeta = np.append(delta_zeta, 0)
+
+        else:
+            sigma = np.sqrt((R*T) / val) #eigenvalues represent spring constant with units of (kcal/mol)/A^2
+            mu = coord #coordinate is assumed to be equilibrium position
+            sample = np.random.normal(loc=mu, scale=sigma, size=1)
+            delta_zeta = np.append(delta_zeta, sample - mu) #sample from normal distribution representing probability of observing system at a given configuration
+
+    noised_coords = np.add(coords, np.dot(eigenvecs, delta_zeta))
+    noised_coords = noised_coords.reshape(int(len(noised_coords) / 3), 3)
     
+    noised_backbone = AtomGroup('protein')
+    noised_backbone.setCoords(noised_coords)
+    noised_backbone.setResnums(backbone.getResnums())
+    noised_backbone.setResnames(backbone.getResnames())
+    noised_backbone.setNames(backbone.getNames())
 
-
-    pass
+    return noised_backbone
