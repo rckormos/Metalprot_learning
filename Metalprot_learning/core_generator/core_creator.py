78a2a398ae41a018fced63fb947504e12b87f0a6
jzhang1198 <jzhang1198@gmail.com>
2022-05-29 19:20:44 -0700

    Dictionary now contains coordinate info

--- Metalprot_learning/core_generator/core_creator.py
+++ Metalprot_learning/core_generator/core_creator.py
@@ -35,14 +35,14 @@ def test_permutation(features, max_permutations):
     if False in set({dimensionality_test, permutation_test}):
         raise utils.PermutationError
 
-def construct_training_example(pdb_file: str, output_dir: str, no_neighbors=1, coordinating_resis=4):
+def construct_training_example(pdb_file: str, output_dir: str, permute: bool, no_neighbors=1, coordinating_resis=4):
     """For a given pdb file, constructs a training example and extracts all features.
 
     Args:
         pdb_file (str): Path to input pdb file.
         output_dir (str): Path to output directory.
         no_neighbors (int, optional): Number of neighbors in primary sequence to coordinating residues be included in core. Defaults to 1.
-        coordinating_resis (int, optional): Sets a threshold for maximum number of metal coordinating residues. Defaults to 4.
+        coordinating_resis (int, optional): Sets  a threshold for maximum number of metal coordinating residues. Defaults to 4.
     """
 
     max_resis = (2*coordinating_resis*no_neighbors) + coordinating_resis
@@ -57,19 +57,24 @@ def construct_training_example(pdb_file: str, output_dir: str, no_neighbors=1, c
     completed = 0
     for core, name in zip(unique_cores, unique_names):
 
-        full_dist_mat, binding_core_identifiers, label = core_featurizer.compute_distance_matrices(core, name, no_neighbors, coordinating_resis)
+        full_dist_mat, binding_core_identifiers, label, metal_coords = core_featurizer.compute_distance_matrices(core, name, no_neighbors, coordinating_resis)
         encoding = core_featurizer.onehotencode(core, no_neighbors, coordinating_resis)
         test_featurization(full_dist_mat, label, encoding, max_resis)
 
         #permute distance matrices, labels, and encodings
-        features = core_permuter.permute_fragments(full_dist_mat, encoding, label, binding_core_identifiers)
-        test_permutation(features, max_permutations)
+        if permute:
+            features = core_permuter.permute_fragments(full_dist_mat, encoding, label, binding_core_identifiers)
+            test_permutation(features, max_permutations)
+
+        else:
+            features = {'observations': full_dist_mat, 'labels': label, 'identifiers': binding_core_identifiers}
 
         #write files to disk
         metal_chid = core.select(f'name {name}') .getChids()[0]
         metal_resnum = core.select(f'name {name}').getResnums()[0]
         filename = core.getTitle() + '_' + '_'.join([str(tup[0]) + tup[1] for tup in binding_core_identifiers]) + '_' + name + str(metal_resnum) + metal_chid
         features['source'] = os.path.join(output_dir, filename + '_core.pdb.gz')
+        features['metal_coords'] = metal_coords
 
         writePDB(os.path.join(output_dir, filename + '_core.pdb.gz'), core)
         with open(os.path.join(output_dir, filename + '_features.pkl'), 'wb') as f:
