dbb6c0a352ae68f222d8498938e83092f576b4e6
jzhang1198 <jzhang1198@gmail.com>
2022-06-19 09:12:22 -0700

    Update

--- Metalprot_learning/tune.py
+++ Metalprot_learning/tune.py
@@ -32,17 +32,17 @@ def define_objective(path2output: str, features_file: str, config: dict, encodin
         os.mkdir(trial_dir)
 
         seed = np.random.randint(0,1000)
-        batch_size = trial.suggest_int("batch_size", config['batch_size'][0], config['batch_size'][1]) if len(config['batch_size']) == 2 else config['batch_size']
-        epochs = trial.suggest_int("epochs", config['epochs'][0], config['epochs'][1]) if len(config['epochs']) == 2 else config['epochs']
-        lr = trial.suggest_float('lr', config['lr'][0], config['lr'][1]) if len(config['lr']) == 2 else config['lr']
-        input_dropout = trial.suggest_float('input_dropout', config['input_dropout'][0], config['input_dropout'][1]) if len(config['input_dropout']) == 2 else config['input_dropout']
-        hidden_dropout = trial.suggest_float('hidden_dropout', config['hidden_dropout'][0], config['hidden_dropout'][1]) if len(config['hidden_dropout']) == 2 else config['hidden_dropout']
-        alpha = trial.suggest_float('weight_decay', config['weight_decay'][0], config['weight_decay'][1]) if len(config['weight_decay']) == 2 else config['weight_decay']
-        l1 = trial.suggest_int("l1", config['l1'][0], config['l1'][1]) if len(config['l1']) == 2 else config['l1']
-        l2 = trial.suggest_int("l2", config['l2'][0], config['l2'][1]) if len(config['l2']) == 2 else config['l2']
+        batch_size = trial.suggest_int("batch_size", config['batch_size'][0], config['batch_size'][1]) if type(config['batch_size']) == tuple else config['batch_size']
+        epochs = trial.suggest_int("epochs", config['epochs'][0], config['epochs'][1]) if type(config['epochs']) == tuple else config['epochs']
+        lr = trial.suggest_float('lr', config['lr'][0], config['lr'][1]) if type(config['lr']) == tuple else config['lr']
+        input_dropout = trial.suggest_float('input_dropout', config['input_dropout'][0], config['input_dropout'][1]) if type(config['input_dropout']) == tuple else config['input_dropout']
+        hidden_dropout = trial.suggest_float('hidden_dropout', config['hidden_dropout'][0], config['hidden_dropout'][1]) if type(config['hidden_dropout']) == tuple else config['hidden_dropout']
+        alpha = trial.suggest_float('weight_decay', config['weight_decay'][0], config['weight_decay'][1]) if type(config['weight_decay']) == tuple else config['weight_decay']
+        l1 = trial.suggest_int("l1", config['l1'][0], config['l1'][1]) if type(config['l1']) == tuple else config['l1']
+        l2 = trial.suggest_int("l2", config['l2'][0], config['l2'][1]) if type(config['l2']) == tuple else config['l2']
 
         if 'l3' in config.keys():
-            l3 = trial.suggest_int("l3", config['l3'][0], config['l3'][1]) if len(config['l3']) == 2 else config['l3']
+            l3 = trial.suggest_int("l3", config['l3'][0], config['l3'][1]) if type(config['l3']) == tuple else config['l3']
             model = models.DoubleLayerNet(input_dim, l1, l2, l3, output_dim, input_dropout, hidden_dropout)
             selected = {'seed': seed, 'batch_size': batch_size, 'lr': lr, 'input_dropout': input_dropout, 'hidden_dropout': hidden_dropout, 'weight_decay': alpha, 'l1': l1, 'l2': l2, 'l3': l3}
 
@@ -57,7 +57,7 @@ def define_objective(path2output: str, features_file: str, config: dict, encodin
         optimizer = torch.optim.SGD(model.parameters(), lr=lr, weight_decay=alpha)
         loss_fn = torch.nn.L1Loss()
 
-        train_dataloader, test_dataloader, _ = train.load_data(features_file, (0.8,0.1,0.1), batch_size, seed, False)
+        train_dataloader, test_dataloader, _, _ = train.load_data(features_file, (0.8,0.1,0.1), batch_size, seed, encodings)
 
         train_loss = np.array([])
         test_loss = np.array([])
