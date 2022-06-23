b1fd6af67bb1f3b5f0b6f6cb0d55a7517d0ad64a
jzhang1198 <jzhang1198@gmail.com>
2022-06-22 17:56:27 -0700

    Added function for initializing runs

--- job_scripts/train.py
+++ job_scripts/train.py
@@ -36,30 +36,50 @@ def distribute_tasks(MODELS: list):
     tasks = [MODELS[i] for i in range(0,len(MODELS)) if i % no_jobs == job_id]
     return path2output, tasks
 
+def configure_run(models: dict, cbeta: bool, encodings: bool, noise: bool, coordination_number: tuple):
+
+    encoding_dim = 20*12 if encodings else 0
+    distance_dim = 3600 if cbeta else 2304
+    total_dim = encoding_dim + distance_dim
+    output_dim = 60 if cbeta else 48
+
+    if cbeta:
+        path2features = '' if coordination_number[0] < 2 else ''
+
+    else:
+        path2features = '' if coordination_number[0] < 2 else ''
+
+    for model in models:
+        model['input'] = total_dim
+        model['output'] = output_dim
+        model['encodings'] = encodings
+        model['noise'] = noise
+
+    return models, path2features
+
 if __name__ == '__main__':
 
-    PATH2FEATURES = '/home/gpu/jzhang1198/data/ZN_binding_cores/datasetV5/compiled_features0.pkl'
+    CBETA = False
+    ENCODINGS = True
+    NOISE = False
+    COORDINATION_NUMBER = (2, 4)
     MODELS = [
-        {'input': 3840,
-        'l1': 2789,
+        {'l1': 2789,
         'l2': 1725,
         'l3': 777,
         'input_dropout': 0.15578875454945562,
         'hidden_dropout': 0.30066048849068494,
         'weight_decay': 0,
-        'output': 60,
         'batch_size': 51,
         'lr': 0.003853602505520586,
         'seed': np.random.randint(1000),
-        'epochs': 2000,
-        'encodings': True,
-        'noise': False}
+        'epochs': 2000}
     ]
 
     path2output, tasks = distribute_tasks(MODELS)
-
+    tasks, path2features = configure_run(tasks, CBETA, ENCODINGS, NOISE)
     for model in tasks:
         today = datetime.datetime.now()
         dirname = os.path.join(path2output, '_'.join(str(i) for i in [today.day, today.month, today.year, today.hour, today.minute, today.second, today.microsecond, model['seed']]))
         os.mkdir(dirname)
-        train_model(dirname, model, PATH2FEATURES)
+        train_model(dirname, model, path2features)
