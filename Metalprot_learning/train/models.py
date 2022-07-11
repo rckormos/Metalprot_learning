a0c7b8b4ed785089f252270da3e86d26acc28716
Jonathan Zhang <jzhang1198@dev1.wynton.ucsf.edu>
2022-07-10 20:35:34 -0700

    Removed old classes

--- Metalprot_learning/train/models.py
+++ Metalprot_learning/train/models.py
@@ -6,34 +6,6 @@ This file contains classes for Metalprot_learning models.
 
 #imports
 import torch
-from collections import OrderedDict
-
-class SingleLayerNetV1(torch.nn.Module):
-    def __init__(self, arch: list):
-        super(SingleLayerNet, self).__init__()
-
-        activation_function_dict = {'ReLU': torch.nn.ReLU()}
-
-        layers = []
-        for ind, layer in enumerate(arch):
-            input_dim = layer['input_dim']
-            output_dim = layer['output_dim']
-            layers.append((f'layer{ind}', torch.nn.Linear(input_dim, output_dim)))
-
-            activation_key = layer['activation']
-            activation = activation_function_dict[activation_key] if activation_key else None
-            if activation:
-                layers.append((f'activation{ind}', activation))
-
-            dropout = torch.nn.Dropout(layer['dropout']) if layer['dropout'] else None
-            if dropout:
-                layers.append(f'droupout{ind}', dropout)
-
-        self.block1 = torch.nn.Sequential(OrderedDict(layers))
-
-    def forward(self, x):
-        y = self.block1(x.float())
-        return y 
 
 class SingleLayerNet(torch.nn.Module):
     def __init__(self, input_dim: int, l1: int, l2: int, output_dim: int, input_dropout: float, hidden_dropout: float):
@@ -47,25 +19,6 @@ class SingleLayerNet(torch.nn.Module):
             torch.nn.Dropout(hidden_dropout),
             torch.nn.ReLU(),
             torch.nn.Linear(l2,output_dim),
-            torch.nn.ReLU()
-        )
-
-    def forward(self, x):
-        y = self.block1(x.float())
-        return y 
-
-class DoubleLayerNeV1(torch.nn.Module):
-    def __init__(self, input_dim: int, l1: int, l2: int, l3: int, output_dim: int):
-        super(DoubleLayerNet, self).__init__()
-
-        self.block1 = torch.nn.Sequential(
-            torch.nn.Linear(input_dim, l1),
-            torch.nn.ReLU(),
-            torch.nn.Linear(l1,l2),
-            torch.nn.ReLU(),
-            torch.nn.Linear(l2,l3),
-            torch.nn.ReLU(),
-            torch.nn.Linear(l3,output_dim),
             torch.nn.ReLU()
         )
 
