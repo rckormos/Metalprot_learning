d69c730f6ccba4dbadb6bec84bbc4d8f4ce728c7
jzhang1198 <jzhang1198@gmail.com>
2022-07-30 13:58:05 -0700

    Update to job submission

--- job_scripts/generate_cores.py
+++ job_scripts/generate_cores.py
@@ -9,22 +9,47 @@ Note that this script is meant to be called from within a SGE bash submission sc
 #imports 
 import os
 import sys
-from Metalprot_learning import data
+from Metalprot_learning import loader
+from Metalprot_learning.utils import AlignmentError, EncodingError
 
 def distribute_tasks(path2examples: str, no_jobs: int, job_id: int):
-    """Distributes pdb files for core generation.
-
-    Args:
-        path2examples (str): Path to directory containing examples.
-        no_jobs (int): Total number of jobs.
-        job_id (int): The job id.
-
-    Returns:
-        tasks (list): list of pdb files assigned to particular job id.
+    """
+    Distributes pdb files across multiple cores for loading.
     """
     pdbs = [os.path.join(path2examples, file) for file in os.listdir(path2examples) if '.pdb' in file]
     tasks = [pdbs[i] for i in range(0, len(pdbs)) if i % no_jobs == job_id]
     return tasks
+
+def run(pdb_file: str, permute: bool):
+    """
+    Wrapper function for running core loading.
+    """
+    try:
+        print(pdb_file)
+        protein = loader.Protein(pdb_file)
+        cores = protein.get_cores()
+        unique_cores = loader.remove_degenerate_cores(cores)
+        for core in unique_cores:
+            core.compute_channels()
+            core.compute_labels()
+
+            if permute:
+                core.permute()
+
+            core.write_pdb_files(path2output)
+            core.write_data_files(path2output)
+        failed_file_line = None
+
+    except AlignmentError as e:
+        failed_file_line = pdb_file + ' Error identifying unique cores'
+
+    except EncodingError as e:
+        failed_file_line = pdb_file + ' Unrecognized amino acid during sequence encoding'
+
+    except:
+        failed_file_line = pdb_file + ' Unknown error occured'
+
+    return failed_file_line
 
 if __name__ == '__main__':
     path2output = sys.argv[1] #path to store outputs    
@@ -41,18 +66,8 @@ if __name__ == '__main__':
     failed = []
     tasks = distribute_tasks(PATH2EXAMPLES, no_jobs, job_id)
     for pdb_file in tasks:
-        protein = data.Protein(pdb_file)
-        cores = protein.get_cores()
-        unique_cores = data.remove_degenerate_cores(cores)
-        for core in unique_cores:
-            core.compute_channels()
-            core.compute_labels()
-
-            if PERMUTE:
-                core.permute()
-
-            core.write_pdb_files(path2output)
-            core.write_data_files(path2output)
+        failed_file_line = run(pdb_file, PERMUTE)
+        failed.append(failed_file_line)
         
     failed = list(filter(None, failed))
     if len(failed) > 0:
