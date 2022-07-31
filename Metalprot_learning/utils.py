f79fac908df6d7569cd229d7316995784eecbbd6
jzhang1198 <jzhang1198@gmail.com>
2022-07-31 11:21:26 -0700

    Removed unecessary child classes

--- Metalprot_learning/utils.py
+++ Metalprot_learning/utils.py
@@ -11,27 +11,3 @@ class Error(Exception):
 class AlignmentError(Error):
     """Raised when identification of unique cores fails"""
     pass
-
-class CoreLoadingError(Error):
-    """Raised when cores are not loaded successfully"""
-    pass
-
-class NoCoresError(Error):
-    """Raised when no cores are found"""
-    pass
-
-class NoisingError(Error):
-    """Raised when core noising was done improperly"""
-    pass
-
-class FeaturizationError(Error):
-    """Raised when featurization fails"""
-    pass
-
-class EncodingError(Error):
-    """Raised when at least one amino acid within the core sequence is unrecognized"""
-    pass 
-
-class PermutationError(Error):
-    """Raised when permutations are done incorrectly"""
-    pass 
