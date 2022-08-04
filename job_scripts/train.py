bb0338943a4f493ac647ba59cb44dd7debfd363e
jzhang1198 <jzhang1198@gmail.com>
2022-08-04 14:03:06 -0700

    Accepted changes

--- job_scripts/train.py
+++ job_scripts/train.py
@@ -36,24 +36,6 @@ def distribute_tasks(MODELS: list):
     tasks = [MODELS[i] for i in range(0,len(MODELS)) if i % no_jobs == job_id]
     return path2output, tasks
 
-<<<<<<< HEAD
-    #define hyperparameters. if you would like to implement a grid search, simply add more values to the lists
-    epochs = [1]
-    batch_sizes = [1000]
-    learning_rates = [0.01]
-    loss_functions = ['MAE'] #can be mean absolute error (MAE) or mean squared error (MSE)
-    optimizers = ['SGD'] #currently can only be stochastic gradient descent (SGD)
-    partition = (0.8,0.1,0.1)
-    seed = 42
-
-    #load data
-    training_data, testing_data, validation_data = split_data(path2observations, path2labels, partition, seed)
-
-    #distribute and run tasks
-    tasks = distribute_tasks(no_jobs, job_id, epochs, batch_sizes, learning_rates, loss_functions, optimizers)
-    for combination in tasks:
-        run_train(path2output, combination)
-=======
 if __name__ == '__main__':
     PATH2FEATURES = '/home/gpu/jzhang1198/data/ZN_binding_cores/cores-2022-07-10/compiled_features0.pkl'
     MODELS = [
@@ -96,4 +78,3 @@ if __name__ == '__main__':
         os.mkdir(dirname)
         train_model(dirname, model, PATH2FEATURES)
         print(f'Execution time: {(datetime.datetime.now() - today)/ (60**2)} hours')
->>>>>>> d69c730f6ccba4dbadb6bec84bbc4d8f4ce728c7
