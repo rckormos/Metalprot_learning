ef425da8c339eec10c0d0a028b2e9a1e318e8a14
jzhang1198 <jzhang1198@gmail.com>
2022-08-04 11:57:11 -0700

    Update to split function

--- Metalprot_learning/train/datasets.py
+++ Metalprot_learning/train/datasets.py
@@ -5,11 +5,13 @@ This file contains functions for loading and splitting data for model training a
 """
 
 #imports
-import pandas as pd
+import os
+import json
 import numpy as np
-import torch
+import pandas as pd
+from torch.utils.data import Dataset
 
-class ImageSet(torch.utils.data.Dataset):
+class ImageSet(Dataset):
     "Custom dataset class for CNN image data."
     def __init__(self, df: pd.DataFrame, encodings: bool):
         self.labels = np.vstack([array for array in df['labels']])
@@ -23,15 +25,16 @@ class ImageSet(torch.utils.data.Dataset):
         label = self.labels[index]
         return observation, label 
 
-def split_data(features_file: str, partitions: tuple, seed: int):
+def split_data(features_file: str, path2output: str, partitions: tuple, seed: int):
     """
-    Splits data into training and test sets.
+    Splits data into training and test sets. Returns a tuple of train, test, and validation dataframes, in that order.
     """
     data = pd.read_pickle(features_file)
     barcodes = np.unique(np.array(data['barcodes']))
+    np.random.seed(seed)
     shuffled_barcodes = np.random.shuffe(barcodes)
 
     train_len, test_len = round(len(data) * partitions[0]), round(len(data) * partitions[1])
     train_barcodes, test_barcodes, val_barcodes = shuffled_barcodes[0: train_len], shuffled_barcodes[train_len: train_len + test_len], shuffled_barcodes[train_len + test_len: len(data)]
-    
-
+    json.dump({'train': train_barcodes, 'test': test_barcodes, 'val': val_barcodes}, os.path.join(path2output, 'barcodes.json'))
+    return data[data['barcodes'].isin(train_barcodes)], data[data['barcodes'].isin(test_barcodes)], data[data['barcodes'].isin(val_barcodes)]
