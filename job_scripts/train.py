630f62bf7b3603b5beca8a0b9f5ec47822ad0d7b
jzhang1198 <jzhang1198@gmail.com>
2022-08-05 11:47:53 -0700

    Update to config dict structure

--- job_scripts/train.py
+++ job_scripts/train.py
@@ -38,21 +38,21 @@ def distribute_tasks(MODELS: list):
 if __name__ == '__main__':
     PATH2FEATURES = '/home/gpu/jzhang1198/data/ZN_binding_cores/cnn-cores-2022-07-31/compiled_data.pkl'
     MODELS = [
-    {
-        'block_n1': {'pin': 40, 'pout': 8, 'kernel_size': 3, 'padding': 1, 'dropout': 0.2},
-        'block0': {'pin': 12, 'pout': 64, 'kernel_size': 3, 'padding': 1, 'dropout': 0.3},
-        'block1': {'pin_residual': 64, 'dilation_residual': 1, 'pin_conv': 64, 'pout_conv': 128, 'kernel_size_conv': 1, 'padding_conv': 0, 'kernel_size_pool': (2,2),'dropout': 0.2},
-        'block2': {'pin_residual': 128, 'dilation_residual': 1, 'pin_conv': 128, 'pout_conv': 256, 'kernel_size_conv': 1, 'padding_conv': 0, 'kernel_size_pool': (2,2),'dropout': 0.2},
-        'block3': {'pin_residual': 256, 'dilation_residual': 1, 'dropout': 0.2},
-        'linear1': {'in': 2304, 'out': 512},
-        'linear2': {'in': 512, 'out': 256},
-        'linear3': {'in': 256, 'out': 48},
-        'encodings': True,
-        'batch_size': 16,
-        'seed': 69,
-        'lr': 0.0001,
-        'epochs': 1
-    }
+        {
+            'block_n1': {'in': 40, 'out': 8, 'kernel_size': 3, 'padding': 1, 'dropout': 0.2},
+            'block0': {'out': 64, 'kernel_size': 3, 'padding': 1, 'dropout': 0.3},
+            'block1': {'dilation_residual': 1, 'out': 128, 'kernel_size_conv': 1, 'padding_conv': 0, 'kernel_size_pool': (2,2),'dropout': 0.2},
+            'block2': {'dilation_residual': 1, 'out': 256, 'kernel_size_conv': 1, 'padding_conv': 0, 'kernel_size_pool': (2,2),'dropout': 0.2},
+            'block3': {'dilation_residual': 1, 'dropout': 0.2},
+            'linear1': {'out': 512},
+            'linear2': {'out': 256},
+            'linear3': {'out': 48},
+            'encodings': True,
+            'batch_size': 16,
+            'seed': 69,
+            'lr': 0.0001,
+            'epochs': 1
+        }
     ]
 
     path2output, tasks = distribute_tasks(MODELS)
