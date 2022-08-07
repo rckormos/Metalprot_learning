7d7f50f26fa17e26eee7e10ee04e87540bc92b36
jzhang1198 <jzhang1198@gmail.com>
2022-08-06 21:46:55 -0700

    Added scaffold code

---
+++ run_prediction.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Author: Jonathan Zhang <jon.zhang@ucsf.edu>
+Script for running metal binding site prediction pipeline.
+"""
+
+#imports
+import os
+import sys
+import torch
+import numpy as np
+from Metalprot_learning import loader
+
+def distribute_tasks(path2pdbs: str, no_jobs: int, job_id: int):
+    """
+    Distributes pdb files across multiple cores for loading.
+    """
+    pdbs = [os.path.join(path2pdbs, file) for file in os.listdir(path2pdbs) if '.pdb' in file]
+    tasks = [pdbs[i] for i in range(0, len(pdbs)) if i % no_jobs == job_id]
+    return tasks
+
+def instantiate_models():
+    classifier = None
+    regressor = None
+    return classifier, regressor
+
+if __name__ == '__main__':
+    path2output = sys.argv[1] #path to store outputs    
+    no_jobs = 1
+    job_id = 0
+
+    if len(sys.argv) > 3:
+        no_jobs = int(sys.argv[2])
+        job_id = int(sys.argv[3]) - 1
+
+    PATH2PDBS = ''
+    tasks = distribute_tasks(PATH2PDBS, no_jobs, job_id)
+    sources, identifiers, features = [], [], []
+    for pdb_file in tasks:
+        print(pdb_file)
+        protein = loader.Protein(pdb_file)
+        cores = protein.get_cores_unknown()
+        _features = np.stack([core.compute_channels() for core in cores], axis=0)
+        sources += [pdb_file] * len(cores)
+        identifiers += [core.identifiers for core in cores]
+        features.append(_features)
+    features = np.stack(features, axis=0)
+    identifiers = np.array(identifiers)
+    sources = np.array(sources)
+    
+    classifier, regressor = instantiate_models()
+    classifications = classifier.forward(torch.from_numpy(features)).cpu().detach().numpy().round()
+    metal_sites_inds = np.argwhere(classifications == 1).flatten()
+
+    regressions = regressor.forward(torch.from_numpy(features[metal_site_inds])).cpu().detach().numpy().round()
+    #triangulate to get coords and confidences
+
+
+    
+
+    
+
