5671f6f2fddf456c81a3d94c8ffd31018104bb16
jzhang1198 <jzhang1198@gmail.com>
2022-08-28 21:41:30 -0700

    Added scaffold for multi task learning class

--- Metalprot_learning/train/models.py
+++ Metalprot_learning/train/models.py
@@ -140,3 +140,29 @@ class AlphafoldNet(nn.Module):
         x = F.relu(self.linear2(x))
         x = self.linear3(x)
         return x
+
+class FullyConnectedNet(torch.nn.Module):
+    def __init__(self, input_dim: int, l1: int, l2: int, l3: int, output_dim: int, input_dropout: float, hidden_dropout: float):
+        super(FullyConnectedNet, self).__init__()
+
+        self.block1 = torch.nn.Sequential(
+            torch.nn.Linear(input_dim, l1),
+            torch.nn.Dropout(input_dropout),
+            torch.nn.ReLU(),
+            torch.nn.Linear(l1,l2),
+            torch.nn.Dropout(hidden_dropout),
+            torch.nn.ReLU(),
+            torch.nn.Linear(l2,l3),
+            torch.nn.Dropout(hidden_dropout),
+            torch.nn.ReLU(),
+            torch.nn.Linear(l3,output_dim),
+            torch.nn.ReLU()
+        )
+
+    def forward(self, x):
+        y = self.block1(x.float())
+        return y.float()
+
+class MultiClassModel(torch.nn.Module):
+    def __init__(self):
+        super(MultiClassModel, self).__init__()
